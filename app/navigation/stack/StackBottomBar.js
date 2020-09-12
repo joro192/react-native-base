f239ce3e649d57ce82f92cd62185c1fc0213a17e
joro192 <joro192@HIEUs-iMac.local>
2020-09-12 16:41:47 +0700

    fix babel

--- app/navigation/stack/StackBottomBar.js
+++ app/navigation/stack/StackBottomBar.js
@@ -1,4 +1,4 @@
-import {SCREEN_ROUTER} from '@constant';
+import {SCREEN_ROUTER} from '@app/constants/Constant';
 import {UserScreen, HomeScreen} from '@screen';
 const { HOME, USER } = SCREEN_ROUTER;
 module.exports = {
